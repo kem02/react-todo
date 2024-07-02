baf3c8efe9fd5aeaede342e4da58bdeee52b6eae
kem02 <113561273+kem02@users.noreply.github.com>
2024-07-02 00:46:57 -0400

    Added DELETE function for Airtable API

--- src/App.jsx
+++ src/App.jsx
@@ -109,6 +109,35 @@ function App() {
 
   }
 
+
+  const deleteTodo = async (id) => {
+
+    const options = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      }
+    };
+
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}/${id}`
+
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.status) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const dataResponse = await response.json();
+
+      return dataResponse;
+
+    } catch (error) {
+      console.log(error.message);
+    }
+
+  };
+
   useEffect(() => {
 
     fetchData();
@@ -150,10 +179,27 @@ function App() {
   }
 
   const removeTodo = (id) => {
+    deleteTodo(id)
     const newDeletedTodo = todoList.filter((todo) => todo.id !== id)
 
     setTodoList(newDeletedTodo)
   }
+
+  const NextPage = () => (
+    <div className={style.nextPage}>
+      <h1>New Todo List Under Construction</h1>
+    </div>
+  )
+  /* 
+    Can also use jsx element instead of component:
+
+    const nextPage = (
+      <div className={style.nextPage}>
+        <h1>New Todo List Under Construction</h1>
+      </div>
+    );
+
+  */
 
   return (
     <BrowserRouter>
@@ -165,7 +211,7 @@ function App() {
             {isLoading ? <p>Loading...</p> : <TodoList todoList={todoList} onRemoveTodo={removeTodo} />}
           </div>
         } />
-        <Route path="/new" element={<h1>New Todo List</h1>} />
+        <Route path="/new" element={<NextPage/>} />
       </Routes>
     </BrowserRouter>
   )
